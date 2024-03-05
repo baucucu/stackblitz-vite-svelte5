2c8706d10b7c0fd20b8781ed22a1da5c736d0a82
Alex Raduca <alexandru.raduca@gmail.com>
2024-03-05 22:36:09 +0200

    working on auth

---
+++ food-flow/src/lib/components/AuthGate.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { browser } from '$app/environment';
+    import { goto } from '$app/navigation';
+    
+    export let passcondition:boolean;
+    export let logout:boolean=true;
+    export let redirect:string="/settings/profile";
+    
+    onMount(async () => { if(browser && !passcondition) {
+        if(logout) {
+            // Cleanup what ever stores and databases you must
+            goto("/");
+        }
+        else goto(redirect);        
+    }});
+    </script>
+    
+    {#if passcondition} <slot /> {/if}
--- food-flow/src/routes/+page.svelte
+++ food-flow/src/routes/+page.svelte
@@ -4,19 +4,19 @@
     async function login() {
         console.log("login");
         await pb.collection('users').authWithOAuth2({ provider: 'google' });
-        console.log(pb.authStore.isValid);
-        console.log(pb.authStore.token);
-        console.log(pb.authStore.model.id);
+        // console.log(pb.authStore.isValid);
+        // console.log(pb.authStore.token);
+        console.log(pb.authStore.model);
     }
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 {#if pb.authStore.isValid}
     <h2>Logged in</h2>
-    <p>Token: {pb.authStore.token}</p>
+    <!-- <p>Token: {pb.authStore.token}</p> -->
     <p>User ID: {pb.authStore.model.id}</p>
     <button on:click={() => pb.authStore.clear()}>Logout</button>
-    {:else}
-        <h2>Not logged in</h2>
-        <button on:click={() => login()}>Login</button>
+{:else}
+    <h2>Not logged in</h2>
+    <button on:click={() => login()}>Login</button>
 {/if}
